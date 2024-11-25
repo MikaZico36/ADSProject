4a66299db1dd8d4190b11ea12c875c6c09dd8863
RafaelGesero <racgo@iscte-iul.pt>
2024-11-25 20:39:24 +0000

    leitura de ficheiros .gpkg  e .xml

--- src/app.py
+++ src/app.py
@@ -1,39 +1,72 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
-import geopandas as gpd
-from shapely.geometry import MultiPolygon
-from shapely.wkt import loads
+from functions import *
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'data_files/input_files'
 
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
 
-@app.route('/upload', methods=['GET','POST'])
+@app.route('/list_files', methods=['GET'])
+def list_files():
+    upload_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'])
+    files = os.listdir(upload_folder)
+    return render_template('list_files.html', files=files)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data
-        filename = secure_filename(file.filename)
-        
-        # Verificar se o ficheiro tem a extensão .gpkg
-        if not filename.lower().endswith('.gpkg'):
-            return "Invalid file type. Only .gpkg files are allowed.", 400
-        
         upload_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'])
-          
-        file_path = os.path.join(upload_folder, filename)
-        file.save(file_path)  # Then save the file
-        return "File has been uploaded."
-    return render_template('index.html', form=form)
+        file_path = save_file(file, upload_folder)
+        
+        if isinstance(file_path, tuple):  # Verifica se houve erro no save_file
+            return file_path
+        
+        # Usar a função list_layers se o ficheiro for .gpkg
+        if file.filename.lower().endswith('.gpkg'):
+            layers = list_layers(file_path)
+            if layers is not None:
+                return render_template('popup.html', message="File has been uploaded and layers listed successfully.")
+            else:
+                return render_template('popup.html', message="Failed to list layers in the GPKG file.")
+        
+        return render_template('popup.html', message="File has been uploaded.")
+    return render_template('read_files.html', form=form)
+
+
+@app.route('/process_file/<filename>', methods=['GET'])
+def process_file(filename):
+    upload_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'])
+    file_path = os.path.join(upload_folder, filename)
+    
+    # Usar a função list_layers se o ficheiro for .gpkg
+    if filename.lower().endswith('.gpkg'):
+        layers = list_layers(file_path)
+        if layers is not None:
+            return jsonify({"layers": layers})
+        else:
+            return jsonify({"error": "Failed to list layers in the GPKG file"}), 500
+    
+    return "File has been selected."
+
+
+
+
 
 
 if __name__ == '__main__':
