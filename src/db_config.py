1c452f28274e567ac04a7b15f41c0d971133f69e
RafaelGesero <racgo@iscte-iul.pt>
2024-12-12 13:27:30 +0000

    estrutura em grafo das propriedades criada

---
+++ src/db_config.py
@@ -0,0 +1,10 @@
+sql_config = {
+    "host": "localhost",
+    "user": "root",
+    "password": "",
+}
+neo4j_config = {
+    "uri": "bolt://44.223.74.103:7687",
+    "username": "neo4j",
+    "password":"crop-halts-starboard"
+}
--- src/functions.py
+++ src/functions.py
@@ -9,6 +9,10 @@ import fiona
 import json
 import geojson
 from neo4j import GraphDatabase
+import mysql.connector
+from db_config import sql_config, neo4j_config
+import numpy as np
+from concurrent.futures import ThreadPoolExecutor
 
 
 def save_file(file, upload_folder):
@@ -42,62 +46,106 @@ def create_relationship_dictionay(file_path):
 
     engine = AdjacencyEngine(source_geoms, **{"max_distance": 100})
     adjacency_dict = engine.get_adjacency_dict()
+    print("criado")
     return adjacency_dict
 
-def get_all_features(file_path):
-    with open(file_path) as f:
-        data = geojson.load(f)
-        return data.get('features', [])
+def create_graph_in_neo4j(batch, graph):
+    uri = neo4j_config["uri"]
+    username = neo4j_config["username"]
+    password = neo4j_config["password"]
 
-def count_features(file_path):
-    with open(file_path) as f:
-        data = geojson.load(f)
-        return len(data['features'])
-
-
-def create_graph_with_features(graph, features):
-    uri = "bolt://44.204.212.169:7687"  
-    username = "neo4j"
-    password = "merchandise-yolks-telecommunication"
     driver = GraphDatabase.driver(uri, auth=(username, password))
-    
     with driver.session() as session:
-        for node, neighbors in graph.items():
-            feature = features[int(node)]  
-            properties = feature['properties']
-            geometry = feature['geometry']
-
-            object_id = properties.get('OBJECTID', None)
-            shape_area = properties.get('Shape_Area', None)
-            shape_length = properties.get('Shape_Length', None)
-            multipolygon = geometry.get('coordinates', None)
-
-            session.run("""
-                MERGE (n:Node {name: $node})
-                SET n.object_id = $object_id,
-                    n.shape_area = $shape_area,
-                    n.shape_length = $shape_length,
-                    n.multipolygon = $multipolygon
-            """, node=node, object_id=object_id, shape_area=shape_area,
-                shape_length=shape_length, multipolygon=str(multipolygon))
-
-            for neighbor in neighbors:
+        for node in batch:
+            print(f"Processing node {node}")
+            session.run("MERGE (:Node {id: $node})", node=node)
+            for neighbor in graph[node]:
                 session.run("""
-                    MATCH (a:Node {name: $node}), (b:Node {name: $neighbor})
+                    MATCH (a:Node {id: $node}), (b:Node {id: $neighbor})
                     MERGE (a)-[:CONNECTED_TO]->(b)
-                """, node=node, neighbor=str(neighbor))
-
+                """, node=node, neighbor=neighbor)
     driver.close()
-    print("Graph created successfully")
 
+def create_thread_pool(graph):
+    nodes = list(graph.keys())
+    batch_size = 100
+
+    batches = [nodes[i:i + batch_size] for i in range(0, len(nodes), batch_size)]
+
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(create_graph_in_neo4j, batch, graph) for batch in batches]
+        for future in futures:
+            try:
+                future.result()
+            except Exception as e:
+                print(f"Erro ao processar um lote: {e}")
+
+
+def get_features(file_path):
+    with open(file_path) as f:
+        data = geojson.load(f)
+        features = data.get("features", [])
+    return features
+
+
+def create_database_and_table():
+    conn = mysql.connector.connect(**sql_config)
+    cursor = conn.cursor()
+
+    cursor.execute("CREATE DATABASE IF NOT EXISTS ads")
+    cursor.execute("USE ads")
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS features (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            shape_length FLOAT,
+            shape_area FLOAT,
+            geometry JSON,
+            node_id INT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def insert_features(features):
+    conn = mysql.connector.connect(**sql_config)
+    cursor = conn.cursor()
+    cursor.execute("USE ads")
+
+    cursor.execute("TRUNCATE TABLE features")
+    
+    for feature in features:
+
+        properties = feature.get("properties", {})
+        geometry = feature.get("geometry", {})
+
+        shape_length = properties.get("Shape_Length")
+        shape_area = properties.get("Shape_Area")
+
+        insert_stm = ("INSERT INTO features (shape_length, shape_area, geometry) VALUES (%s, %s, %s)")
+        data = (shape_length, shape_area, json.dumps(geometry))
+
+        cursor.execute(insert_stm, data)
+    
+    cursor.execute("UPDATE features SET node_id = id - 1")
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def process_geojson_to_sql(file_path):
+    features = get_features(file_path)
+    create_database_and_table()
+    insert_features(features)
 
 def main():
-    file_path = "data_files/input_files/Acores_Grupo_Ocidental_Parcelas_az_oc.geojson"
-    dictionary = create_relationship_dictionay(file_path)
-    print(dictionary)
-    features = get_all_features(file_path)
-    print(features)
-    create_graph_with_features(dictionary, features)
+    file_path = "src/data_files/input_files/Acores_Grupo_Ocidental_Parcelas_az_oc.geojson"
+    process_geojson_to_sql(file_path)
+
+    #graph = create_relationship_dictionay(file_path)
+    #create_thread_pool(graph)
+    
+
 
 if __name__ == "__main__":
     main()
