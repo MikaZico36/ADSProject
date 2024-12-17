a517ec8910147ad5d1e9b1953bf39b5b5e5dfc2b
Miguel Monteiro <mmonteiro03611@gmail.com>
2024-12-17 00:21:20 +0000

    Endpoint trade properties
    Feito a base do método que será responsável por avaliar e realizar as trocas de terrenos

--- src/routes/search_routes.py
+++ src/routes/search_routes.py
@@ -1,4 +1,7 @@
+from re import search
+
 from flask import Blueprint, request, jsonify
+
 from src.services.search_services import *
 
 search_blueprint = Blueprint('search_routes', __name__)
@@ -18,10 +21,40 @@ def fetch_owner_by_id(owner_id):
     try:
         owner = get_owner_by_id(owner_id)
         if owner:
-            return jsonify({"status": "success", "data": owner}), 200
+            return jsonify({"status": "success", "data": owner_id}), 200
         else:
             return jsonify({"status": "error", "message": "Owner not found"}), 404
     except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+
+@search_blueprint.route('/totalArea/<int:owner_id>', methods=['GET'])
+def get_total_area(owner_id):
+    try:
+        total_area = get_total_area_by_owner(owner_id)
+
+        if total_area is not None:
+            return jsonify({"status": "success", "data": {"total_area": total_area}}), 200
+        else:
+            return jsonify({"status": "error", "message": "Owner not found"}), 404
+
+    except Exception as e:
+        print(f"Exception occurred: {str(e)}")
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+
+@search_blueprint.route('/meanArea/<int:owner_id>', methods=['GET'])
+def get_mean_area(owner_id):
+    try:
+        mean_area = get_mean_area_by_owner(owner_id)
+
+        if mean_area is not None:
+            return jsonify({"status": "success", "data": {"total_area": mean_area}}), 200
+        else:
+            return jsonify({"status": "error", "message": "Owner not found"}), 404
+
+    except Exception as e:
+        print(f"Exception occurred: {str(e)}")
         return jsonify({"status": "error", "message": str(e)}), 500
 
 
@@ -39,3 +72,5 @@ def fetch_owner_by_name():
             return jsonify({"status": "error", "message": "Owner not found"}), 404
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
+
+
--- src/services/search_services.py
+++ src/services/search_services.py
@@ -1,5 +1,5 @@
 from neo4j import GraphDatabase
-from src.db_config import neo4j_config
+from src.services.db_config import neo4j_config
 import time
 
 def get_owner_by_id(owner_id):
@@ -59,6 +59,8 @@ def get_owner_with_properties_by_name(name):
     else:
         return None
 
+
+
 def get_property_with_adjacents(property_id):
     driver = GraphDatabase.driver(neo4j_config["uri"], auth=(neo4j_config["username"], neo4j_config["password"]))
 
@@ -85,7 +87,7 @@ def get_property_with_adjacents(property_id):
     else:
         return None
 
-def get_propertie_area(property_id):
+def get_property_area(property_id):
     driver = GraphDatabase.driver(neo4j_config["uri"], auth=(neo4j_config["username"], neo4j_config["password"]))
     with driver.session() as session:
         area = session.run(
@@ -98,21 +100,48 @@ def get_propertie_area(property_id):
         driver.close()
         return area
 
+#Calcula a área total de todos os terrenos de um determinado dono
 def get_total_area_by_owner(owner_id):
     driver = GraphDatabase.driver(neo4j_config["uri"], auth=(neo4j_config["username"], neo4j_config["password"]))
     with driver.session() as session:
-        total_area = session.run("""
-
+        result = session.run(
+            """
             MATCH (o:Owner {owner_id: $owner_id})-[:OWNS]->(p:Property)
-                             RETURN sum(p.shape_area) AS total_area
-            """, 
-            owner_id=owner_id).single()
+            RETURN sum(p.shape_area) AS total_area
+            """,
+            owner_id=owner_id
+        ).single()
+
         driver.close()
-        print(total_area)
-        return total_area
-    
+
+        if result and result["total_area"] is not None:
+            return result["total_area"]
+        return None
 
 
+def get_mean_area_by_owner(owner_id):
+    driver = GraphDatabase.driver(neo4j_config["uri"], auth=(neo4j_config["username"], neo4j_config["password"]))
+    with driver.session() as session:
+        result = session.run(
+            """
+            MATCH (o:Owner {owner_id: $owner_id})-[:OWNS]->(p:Property)
+            RETURN avg(p.shape_area) AS mean_area
+            """,
+            owner_id=owner_id
+        ).single()
+
+        driver.close()
+
+        if result and result["mean_area"] is not None:
+            return result["mean_area"]
+        return None
+
+
+
+
+
+
+#Calcula a área de uma propriedade
 def calculate_total_area(property_ids):
     driver = GraphDatabase.driver(neo4j_config["uri"], auth=(neo4j_config["username"], neo4j_config["password"]))
     
@@ -127,7 +156,8 @@ def calculate_total_area(property_ids):
         ).single()
         
         total_area = result["total_area"] if result and result["total_area"] else 0.0
-    
+        print(total_area)
+
     driver.close()
     return total_area
     
@@ -172,13 +202,91 @@ def get_area_adject_properties_by_owner(owner_id):
         driver.close()
         print(adjacent_property_areas)
         return adjacent_property_areas
-    
+
+get_property_area(1)
+
+def count_same_elements(properties_neighbors, properties_owner):
+    count = sum(1 for item in properties_owner if item in properties_neighbors)
+    return count
+
+
+#TODO fazer a troca onde o terreno menor deve valorizar se tem vizinho e o terreno maior a área
+#TODO Verificar se o terreno no sitio onde está não vai perder vizinhos
+def trade_properties(property1_id, property2_id):
+    driver = GraphDatabase.driver(neo4j_config["uri"], auth=(neo4j_config["username"], neo4j_config["password"]))
+    with driver.session() as session:
+        all_property1_neighbors = get_property_with_adjacents(property1_id)
+        all_property2_neighbors = get_property_with_adjacents(property2_id)
+        property1_area = get_property_area(property1_id)
+        property2_area= get_property_area(property2_id)
+        property1_owner = session.run(
+            """
+            MATCH (o:Owner)-[:OWNS]->(p:Property {object_id: $property1_id})
+            RETURN o.owner_id AS owner_id, o.name AS owner_name
+            """,
+            property1_id=property1_id
+        ).single()
+        property2_owner = session.run(
+            """
+            MATCH (o:Owner)-[:OWNS]->(p:Property {object_id: $property2_id})
+            RETURN o.owner_id AS owner_id, o.name AS owner_name
+            """,
+            property2_id=property2_id
+        ).single()
+
+
+        if property1_area > property2_area:
+
+            property1_owner_name = property1_owner['owner_name']
+            all_properties = get_owner_with_properties_by_name(property1_owner_name)
+
+            number_of_properties_in_commun = count_same_elements(all_property2_neighbors['adjacent_properties'], all_properties['properties'])
+            owner1_points = number_of_properties_in_commun * 100
+            properties_area_diff = get_property_area(property1_id)['area'] - get_property_area(property2_id)['area']
+            print(f"Diferenca de área: {properties_area_diff} | número de vizinhos ganhos: {number_of_properties_in_commun}")
+            if owner1_points >= 100 and properties_area_diff >= 100:
+                driver.close()
+                return True
+            driver.close()
+            return False
+
+        elif property2_area > property1_area:
+            property2_owner_name = property2_owner['owner_name']
+            all_properties = get_owner_with_properties_by_name(property2_owner_name)
+
+            number_of_properties_in_commun = count_same_elements(all_property1_neighbors['adjacent_properties'],
+                                                                 all_properties['properties'])
+            owner2_points = number_of_properties_in_commun * 100
+            properties_area_diff = get_property_area(property2_id)['area'] - get_property_area(property1_id)['area']
+            print(f"Diferenca de área: {properties_area_diff} | número de vizinhos ganhos: {number_of_properties_in_commun}")
+
+            if owner2_points >= 100 and properties_area_diff >= 100:
+                driver.close()
+                return True
+            driver.close()
+            return False
+
+
+
+
+
+
 
 if __name__ == "__main__":
 
     start = time.time()
     
-    get_area_adject_properties_by_owner(1)
-
+    #get_area_adject_properties_by_owner(1)
+    #get_total_area_by_owner(2)
+    a = get_property_with_adjacents(14)
+    print(a)
+    #trade_properties(1,14)
+    print(get_owner_with_properties_by_name('trusting_rhodes'))
+    area1 = get_property_area(14)
+    area2 = get_property_area(411)
+    print("Area1: " + str(area1))
+    print("Area2: " + str(area2))
+    a = trade_properties(14,411)
+    print("Trade properties: " + str(a))
     end = time.time()
     print(f"Tempo total: {end - start}")
